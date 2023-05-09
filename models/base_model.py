a4f51eecf3b7483a58239c7743c834375c3da01a
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-05-09 17:31:31 +0000

    Update models/base_model.py __init__(self, *args, **kwargs) - Using the *args and **kwargs arguments for the constructor of a BaseModel created_at and updated_at gets converted to datetime object Added Tests suites for the changes made to ensure it works properly

--- models/base_model.py
+++ models/base_model.py
@@ -25,12 +25,17 @@ class BaseModel:
     __dict__ of the instance
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This initialises the BaseModel class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            self.id = kwargs['id']
+            self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel Class
