384a57e106a6c2a0fe071899efd9b76cdaac4845
TechOgede <sammonius@live.com>
2023-05-10 03:55:13 -0700

    file_storage implementation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@
     and deserialize objects to and from a file respectively
     '''
 import json
+import os
 
 
 class FileStorage():
@@ -25,19 +26,35 @@ class FileStorage():
 
     def save(self):
         ''' serializes __objects to the JSON file (path: __file_path) '''
+        new_dict = {}
+
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as json_file:
+                json_str = json_file.read()
+            json_str = json_str[:-1]
+            json_str_new = json.dumps(new_dict)[1:]
+            json_str_new = json_str + ', ' + json_str_new
+
+        else:
+            json_str_new = json.dumps(new_dict)
+
         with open(self.__file_path, 'w', encoding='utf-8') as json_file:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                new_dict[key] = value.to_dict()
-            json.dump(new_dict, json_file)
+            json_file.write(json_str_new)
 
     def reload(self):
-        ''' deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)
+        ''' deserializes the JSON file to __objects
+            (only if the JSON file (__file_path) exists)
             if the file doesnt exist, no exception is raised '''
         try:
             json_file = open(self.__file_path, 'r', encoding='utf-8')
-        except:
+        except FileNotFoundError:
             pass
         else:
-            self._objects = json.load(json_file)
+            dict_objs = json.load(json_file)
+            from models.base_model import BaseModel
+            for key, value in dict_objs.items():
+                self.__objects[key] = BaseModel(**value)
             json_file.close()
